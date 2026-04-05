4f5ea9cfdc327fb973fc6f5c17744502705983db
Aditi Singh <211120224+aditiisingh0@users.noreply.github.com>
2026-04-06 03:10:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ fetch('medicines.json')
 .then(response => response.json())
 .then(data => {
     medicines = data;
+})
+.catch(error => {
+    console.log("Error loading medicines:", error);
 });
 
 function searchMedicine() {
@@ -15,11 +18,11 @@ function searchMedicine() {
     if (found) {
         let savings = found.brand_price - found.generic_price;
         resultDiv.innerHTML = `
-            <h3>${found.brand}</h3>
+            <h2>${found.brand}</h2>
             <p><b>Generic:</b> ${found.generic}</p>
             <p>Brand Price: ₹${found.brand_price}</p>
             <p>Generic Price: ₹${found.generic_price}</p>
-            <p style="color:green;"><b>You Save ₹${savings}</b></p>
+            <p style="color:green;font-size:20px;"><b>You Save ₹${savings}</b></p>
             <p>Use: ${found.use}</p>
             <p style="color:blue;"><b>Available at Jan Aushadhi Store</b></p>
         `;
